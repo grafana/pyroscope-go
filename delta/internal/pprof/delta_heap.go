7f12b772c75cb908e6e28d05bc48b21b61d083a1
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-01-17 16:21:39 +0700

    rm debug logging

--- delta/internal/pprof/delta_heap.go
+++ delta/internal/pprof/delta_heap.go
@@ -1,7 +1,6 @@
 package pprof
 
 import (
-	"fmt"
 	"io"
 	"math"
 	"runtime"
@@ -52,36 +51,17 @@ func (d *DeltaHeapProfiler) WriteHeapProto(w io.Writer, p []runtime.MemProfileRe
 		}
 
 		// do the delta
-		pp := func(prefix string, stk []uintptr) {
-			frames1 := runtime.CallersFrames(stk)
-			for {
-				f, more := frames1.Next()
-				if !more {
-					break
-				}
-				fmt.Printf("[dhp] %s %s\n", prefix, f.Func.Name())
-			}
-		}
 		if r.AllocBytes == 0 && r.AllocObjects == 0 && r.FreeObjects == 0 && r.FreeBytes == 0 {
 			// it is a fresh bucket and it will be published after next 1-2 gc cycles
 			continue
 		}
 		size := int64(0)
 		if r.AllocObjects != 0 {
-			if r.AllocBytes%r.AllocObjects != 0 {
-				fmt.Printf("[dhp] warning: %d %d\n", r.AllocObjects, r.AllocBytes)
-				pp("mod", r.Stack())
-			}
 			size = r.AllocBytes / r.AllocObjects
-		} else {
-			fmt.Printf("[dhp] zero\n")
-			pp("zero", r.Stack())
 		}
 		entry := d.m.Lookup(r.Stack(), uintptr(size))
 
 		if (r.AllocObjects - entry.count.v1) < 0 {
-			pp("neg 1", r.Stack())
-			pp("neg 2", entry.stk[:])
 			continue
 		}
 		AllocObjects := r.AllocObjects - entry.count.v1
@@ -95,15 +75,8 @@ func (d *DeltaHeapProfiler) WriteHeapProto(w io.Writer, p []runtime.MemProfileRe
 		if values[0] == 0 && values[1] == 0 && values[2] == 0 && values[3] == 0 {
 			continue
 		}
-		//var blockSize int64
-		//if r.AllocObjects > 0 {
-		//	blockSize = r.AllocBytes / r.AllocObjects
-		//}
-		// todo should we skip if values are 0?
 		b.pbSample(values, locs, func() {
-			//if blockSize != 0 {
-			//	b.pbLabel(tagSample_Label, "bytes", "", blockSize)
-			//}
+
 		})
 	}
 	b.build()
