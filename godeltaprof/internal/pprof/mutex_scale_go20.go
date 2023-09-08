12cdc76a9500d7b20989f197873e8fb80ed63a6c
Tolyan Korniltsev <korniltsev.anatoly@gmail.com>
2023-09-08 19:36:58 +0700

    add comment

--- godeltaprof/internal/pprof/mutex_scale_go20.go
+++ godeltaprof/internal/pprof/mutex_scale_go20.go
@@ -6,6 +6,8 @@ package pprof
 type MutexProfileScaler struct {
 }
 
+// scaleMutexProfile is a no-op for go1.20+.
+// https://github.com/golang/go/commit/30b1af00ff142a3f1a5e2a0f32cf04a649bd5e65
 func scaleMutexProfile(_ MutexProfileScaler, cnt int64, ns float64) (int64, float64) {
 	return cnt, ns
 }
