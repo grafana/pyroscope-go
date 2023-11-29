7fbb20ad894f93c414b9f1e7ae0e87edc7a155fa
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-11-29 10:32:00 +0700

    make reading /proc/self/maps lazy

--- godeltaprof/proto.go
+++ godeltaprof/proto.go
@@ -5,4 +5,5 @@ type ProfileOptions struct {
 	// for go1.21+ if false - use runtime.Frame->Function - produces frames with generic types ommited [...]
 	// pre 1.21 - always use runtime.Frame->Function - produces frames with generic types ommited [...]
 	GenericsFrames bool
+	LazyMappings   bool
 }
