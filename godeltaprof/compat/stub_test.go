491e62797d5fe1be39df10b33662a2f0205711cf
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-05-22 16:16:55 +0700

    fix(godeltaprof): handle renamed runtime.pprof_cyclesPerSecond (#107)

--- godeltaprof/compat/stub_test.go
+++ godeltaprof/compat/stub_test.go
@@ -10,30 +10,6 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-func TestSignatureExpandFinalInlineFrame(t *testing.T) {
-	checkSignature(t, "github.com/grafana/pyroscope-go/godeltaprof/internal/pprof",
-		"runtime_expandFinalInlineFrame",
-		"func github.com/grafana/pyroscope-go/godeltaprof/internal/pprof.runtime_expandFinalInlineFrame(stk []uintptr) []uintptr")
-}
-
-func TestSignatureCyclesPerSecond(t *testing.T) {
-	checkSignature(t, "github.com/grafana/pyroscope-go/godeltaprof/internal/pprof",
-		"runtime_cyclesPerSecond",
-		"func github.com/grafana/pyroscope-go/godeltaprof/internal/pprof.runtime_cyclesPerSecond() int64")
-}
-
-func TestSignatureCyclesPerSecondRuntime(t *testing.T) {
-	checkSignature(t, "runtime/pprof",
-		"runtime_cyclesPerSecond",
-		"func runtime/pprof.runtime_cyclesPerSecond() int64")
-}
-
-func TestSignatureExpandFinalInlineFrameRuntime(t *testing.T) {
-	checkSignature(t, "runtime/pprof",
-		"runtime_expandFinalInlineFrame",
-		"func runtime/pprof.runtime_expandFinalInlineFrame(stk []uintptr) []uintptr")
-}
-
 func checkSignature(t *testing.T, pkg string, name string, expectedSignature string) {
 	cfg := &packages.Config{
 		Mode:  packages.NeedImports | packages.NeedExportFile | packages.NeedTypes | packages.NeedSyntax,
