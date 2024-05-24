f4d5924f3809378bc096168a6e08b0c849d75458
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-05-24 19:20:48 +0700

    improve test, remove internals tests

--- godeltaprof/compat/delta_test.go
+++ godeltaprof/compat/delta_test.go
@@ -1,13 +1,11 @@
 package compat
 
 import (
-	"bytes"
 	"fmt"
-	gprofile "github.com/google/pprof/profile"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/grafana/pyroscope-go/godeltaprof/internal/pprof"
+	"reflect"
 	"runtime"
-	"sort"
+	"strings"
 	"testing"
 )
 
@@ -16,14 +14,47 @@ var (
 	stack0Marker string
 	stack1       = [32]uintptr{}
 	stack1Marker string
+	stack2       = [32]uintptr{}
+	stack2Marker string
+	stack3       = [32]uintptr{}
+	stack4       = [32]uintptr{}
 )
 
 func init() {
 	fs := getFunctionPointers()
+
 	stack0 = [32]uintptr{fs[0], fs[1]}
 	stack1 = [32]uintptr{fs[2], fs[3]}
-	stack0Marker = runtime.FuncForPC(fs[1]).Name() + ";" + runtime.FuncForPC(fs[0]).Name()
-	stack1Marker = runtime.FuncForPC(fs[3]).Name() + ";" + runtime.FuncForPC(fs[2]).Name()
+	stack2 = [32]uintptr{
+		reflect.ValueOf(runtime.GC).Pointer(),
+		reflect.ValueOf(runtime.FuncForPC).Pointer(),
+		reflect.ValueOf(TestDeltaBlockProfile).Pointer(),
+		reflect.ValueOf(TestDeltaHeap).Pointer(),
+	}
+	stack3 = [32]uintptr{ // equal , but difference in runtime
+		reflect.ValueOf(runtime.GC).Pointer() + 1,
+		reflect.ValueOf(runtime.FuncForPC).Pointer(),
+		reflect.ValueOf(TestDeltaBlockProfile).Pointer(),
+		reflect.ValueOf(TestDeltaHeap).Pointer(),
+	}
+
+	stack4 = [32]uintptr{ // equal , but difference in non runtime frame
+		reflect.ValueOf(runtime.GC).Pointer(),
+		reflect.ValueOf(runtime.FuncForPC).Pointer(),
+		reflect.ValueOf(TestDeltaBlockProfile).Pointer() + 1,
+		reflect.ValueOf(TestDeltaHeap).Pointer(),
+	}
+	marker := func(stk []uintptr) string {
+		res := []string{}
+		for i := range stk {
+			f := stk[len(stk)-1-i]
+			res = append(res, runtime.FuncForPC(f).Name())
+		}
+		return strings.Join(res, ";")
+	}
+	stack0Marker = marker(stack0[:2])
+	stack1Marker = marker(stack1[:2])
+	stack2Marker = marker(stack2[:2])
 }
 
 func TestDeltaHeap(t *testing.T) {
@@ -167,224 +198,61 @@ func BenchmarkMutexDelta(b *testing.B) {
 	}
 }
 
-//func TestMutexDuplicates(t *testing.T) {
-//	h := newMutexTestHelper()
-//	const cycles = 42
-//	p := h.dump(
-//		h.r(239, 239*cycles, stack0),
-//		h.r(42, 42*cycles, stack1),
-//		h.r(7, 7*cycles, stack0),
-//	)
-//	expectStackFrames(t, p, stack0Marker, 239+7, (239+7)*cycles)
-//	expectStackFrames(t, p, stack1Marker, 239+7, (239+7)*cycles)
-//
-//	p = h.dump(
-//		h.r(239, 239*cycles, stack0),
-//		h.r(42, 42*cycles, stack1),
-//		h.r(7, 7*cycles, stack0),
-//	)
-//	expectEmptyProfile(t, p)
-//}
-//
-//func TestHeapDuplicates(t *testing.T) {
-//	const testMemProfileRate = 524288
-//	h := newHeapTestHelper()
-//	h.rate = testMemProfileRate
-//	const blockSize = 1024
-//	p := h.dump(
-//		h.r(239, 239*blockSize, 239, 239*blockSize, stack0),
-// todo add a record with the same stack but different values
-//		h.r(42, 42*blockSize, 42, 42*blockSize, stack1),
-//		h.r(7, 7*blockSize, 7, 7*blockSize, stack0),
-//	)
-//	c1, b1 := pprof.ScaleHeapSample(239+7, (239+7)*blockSize, testMemProfileRate)
-//	expectStackFrames(t, p, stack0Marker, c1, b1, 0, 0)
-//	c2, b2 := pprof.ScaleHeapSample(42, 42*blockSize, testMemProfileRate)
-//	expectStackFrames(t, p, stack1Marker, c2, b2, 0, 0)
-//
-//	p = h.dump(
-//		h.r(239, 239*blockSize, 239, 239*blockSize, stack0),
-//		h.r(42, 42*blockSize, 42, 42*blockSize, stack1),
-//		h.r(7, 7*blockSize, 7, 7*blockSize, stack0),
-//	)
-//	expectEmptyProfile(t, p)
-//}
+func TestMutexDuplicates(t *testing.T) {
+	h := newMutexTestHelper()
+	const cycles = 42
+	p := h.dump(
+		h.r(239, 239*cycles, stack0),
+		h.r(42, 42*cycles, stack1),
+		h.r(7, 7*cycles, stack0),
+	)
+	expectStackFrames(t, p, stack0Marker, 239+7, (239+7)*cycles)
+	expectStackFrames(t, p, stack1Marker, 239+7, (239+7)*cycles)
 
-type allocEntry struct {
-	pc     []int64
-	values []int64
-	sample *gprofile.Sample
+	p = h.dump(
+		h.r(239, 239*cycles, stack0),
+		h.r(42, 42*cycles, stack1),
+		h.r(7, 7*cycles, stack0),
+	)
+	expectEmptyProfile(t, p)
 }
 
-func (e *allocEntry) String() string {
-	return fmt.Sprintf("%+v %+v %+v", pprofSampleStackToString(e.sample), e.values, e.sample.NumLabel)
-}
-func (e *allocEntry) String2() string {
-	return fmt.Sprintf("%+v %+v", e.pc, e.values)
-}
-
-func TestChanAllocDup(t *testing.T) {
-	prevRate := runtime.MemProfileRate
-	runtime.MemProfileRate = 1
-	defer func() {
-		runtime.MemProfileRate = prevRate
-	}()
+func TestHeapDuplicates(t *testing.T) {
+	const testMemProfileRate = 524288
 	h := newHeapTestHelper()
-	h.rate = int64(runtime.MemProfileRate)
-
-	tests := []int{0, 1024}
-	profiles := []*bytes.Buffer{
-		bytes.NewBuffer(nil),
-		bytes.NewBuffer(nil),
+	h.rate = testMemProfileRate
+	const blockSize = 1024
+	const blockSize2 = 1024
+	p := h.dump(
+		h.r(239, 239*blockSize, 239, 239*blockSize, stack0),
+		h.r(3, 3*blockSize2, 3, 3*blockSize2, stack0),
+		h.r(42, 42*blockSize, 42, 42*blockSize, stack1),
+		h.r(7, 7*blockSize, 7, 7*blockSize, stack0),
+		h.r(3, 3*blockSize, 3, 3*blockSize, stack2),
+		h.r(5, 5*blockSize, 5, 5*blockSize, stack3),
+		h.r(11, 11*blockSize, 11, 11*blockSize, stack4),
+	)
+	scale := func(c, b int) []int64 {
+		c1, b1 := pprof.ScaleHeapSample(int64(c), int64(b), testMemProfileRate)
+		return []int64{c1, b1, 0, 0}
 	}
-	f := func(i int) {
-		for _, test := range tests {
-			_ = make(chan int, test)
-			_ = make(chan structWithPointers, test) // with pointers
-		}
-		runtime.GC()
-		runtime.GC()
-		p := profiles[i]
-		_ = WriteHeapProto(h.dp, h.opt, p, dumpMemProfileRecords(), h.rate)
-	}
-	for i := 0; i < 2; i++ {
-		f(i)
-	}
-	runtime.MemProfileRate = prevRate
+	expectStackFrames(t, p, stack0Marker, scale(239+7, (239+7)*blockSize)...)
+	expectStackFrames(t, p, stack1Marker, scale(42, 42*blockSize)...)
 
-	const pattern = "^testing.tRunner;github.com/grafana/pyroscope-go/godeltaprof/compat.TestChanAllocDup;github.com/grafana/pyroscope-go/godeltaprof/compat.TestChanAllocDup.func2$"
+	expectPPROFLocations(t, p, fmt.Sprintf("^%s$", stack0Marker), 1, scale(239+7, (239+7)*blockSize)...)
+	expectPPROFLocations(t, p, fmt.Sprintf("^%s$", stack1Marker), 1, scale(42, 42*blockSize)...)
+	expectPPROFLocations(t, p, fmt.Sprintf("^%s$", stack2Marker), 1, scale(3, 3*blockSize)...)
+	expectPPROFLocations(t, p, fmt.Sprintf("^%s$", stack2Marker), 1, scale(5, 5*blockSize)...)
+	expectPPROFLocations(t, p, fmt.Sprintf("^%s$", stack2Marker), 1, scale(11, 11*blockSize)...)
 
-	compareSamplesWithPattern(t, profiles[0], profiles[1], 5, pattern, func(s *gprofile.Sample) bool {
-		return true
-	})
-
-}
-
-func compareSamplesWithPattern(t *testing.T, p0, p1 *bytes.Buffer, gtn int, pattern string, extraFilter func(s *gprofile.Sample) bool) {
-	var entries [][]allocEntry
-	var strEntries [][]string
-	profiles := []*bytes.Buffer{p0, p1}
-	for _, profile := range profiles {
-		pp, err := gprofile.ParseData(profile.Bytes())
-		require.NoError(t, err)
-		samples := grepSamples(pp, pattern)
-		es := []allocEntry{}
-		for _, sample := range samples {
-			if !extraFilter(sample) {
-				continue
-			}
-			e := allocEntry{}
-			for _, location := range sample.Location {
-				e.pc = append(e.pc, int64(location.Address))
-			}
-			e.values = sample.Value
-			e.sample = sample
-			es = append(es, e)
-		}
-		entries = append(entries, es)
-	}
-	cmpi := func(x, y int64) int {
-		if x < y {
-			return -1
-		}
-		if x > y {
-			return +1
-		}
-		return 0
-	}
-	cmp := func(s1, s2 []int64) int {
-		for i, v1 := range s1 {
-			if i >= len(s2) {
-				return +1
-			}
-			v2 := s2[i]
-			if c := cmpi(v1, v2); c != 0 {
-				return c
-			}
-		}
-		if len(s1) < len(s2) {
-			return -1
-		}
-		return 0
-	}
-
-	pcmaps := []map[string]int{}
-
-	for i, es := range entries {
-		sort.Slice(es, func(i, j int) bool {
-			if c := cmp(es[i].pc, es[j].pc); c != 0 {
-				return c < 0
-			}
-			if c := cmp(es[i].values, es[j].values); c != 0 {
-				return c < 0
-			}
-			return false
-		})
-		ss := []string{}
-		fmt.Printf("========================== %d\n", i)
-		pcmap := map[string]int{}
-		for _, e := range es {
-			ss = append(ss, e.String())
-			fmt.Printf("%s\n", e.String())
-			fmt.Printf("%s\n", e.String2())
-			pcmap[fmt.Sprintf("%+v", e.pc)]++
-		}
-		strEntries = append(strEntries, ss)
-		pcmaps = append(pcmaps, pcmap)
-	}
-
-	assert.Equal(t, strEntries[0], strEntries[1])
-	assert.GreaterOrEqual(t, len(strEntries[0]), gtn)
-
-	cnt := 0
-	pcmap := pcmaps[0]
-	for _, v := range pcmap {
-		if v > 1 {
-			cnt++
-		}
-	}
-	assert.Greater(t, cnt, 0)
-}
-
-type structWithPointers struct {
-	t1 *testing.T
-	t2 *testing.T
-}
-
-// todo we should merge these allocations with an option
-func TestMapAlloc(t *testing.T) {
-	prevRate := runtime.MemProfileRate
-	runtime.MemProfileRate = 1
-	defer func() {
-		runtime.MemProfileRate = prevRate
-	}()
-	h := newHeapTestHelper()
-	h.rate = int64(runtime.MemProfileRate)
-
-	profiles := []*bytes.Buffer{
-		bytes.NewBuffer(nil),
-		bytes.NewBuffer(nil),
-	}
-	f := func(i int) {
-		mm := make(map[string]structWithPointers)
-		for i := 0; i < 2048; i++ {
-			k := fmt.Sprintf("k_____%d", i)
-			mm[k] = structWithPointers{t, t}
-		}
-		runtime.GC()
-		runtime.GC()
-		p := profiles[i]
-		_ = WriteHeapProto(h.dp, h.opt, p, dumpMemProfileRecords(), h.rate)
-	}
-	for i := 0; i < 2; i++ {
-		f(i)
-	}
-
-	runtime.MemProfileRate = prevRate
-
-	compareSamplesWithPattern(t, profiles[0], profiles[1], 9, "^testing.tRunner;github.com/grafana/pyroscope-go/godeltaprof/compat.TestMapAlloc;github.com/grafana/pyroscope-go/godeltaprof/compat.TestMapAlloc.func2$", func(s *gprofile.Sample) bool {
-		bs := s.NumLabel["bytes"]
-		return len(bs) == 1 && bs[0] != 288 && bs[0] != 16 // i have no idea what this is, this is to fix fragile test
-	})
-
+	p = h.dump(
+		h.r(239, 239*blockSize, 239, 239*blockSize, stack0),
+		h.r(3, 3*blockSize2, 3, 3*blockSize2, stack0),
+		h.r(42, 42*blockSize, 42, 42*blockSize, stack1),
+		h.r(7, 7*blockSize, 7, 7*blockSize, stack0),
+		h.r(3, 3*blockSize, 3, 3*blockSize, stack2),
+		h.r(5, 5*blockSize, 5, 5*blockSize, stack3),
+		h.r(11, 11*blockSize, 11, 11*blockSize, stack4),
+	)
+	expectEmptyProfile(t, p)
 }
