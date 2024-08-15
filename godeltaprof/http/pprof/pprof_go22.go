c239287d65542eeb252119edcb3e75fc2ae94f32
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-08-15 12:34:19 +0200

    simplify: just use "GET " all the time on go 1.22

--- godeltaprof/http/pprof/pprof_go22.go
+++ godeltaprof/http/pprof/pprof_go22.go
@@ -2,17 +2,9 @@
 
 package pprof
 
-import "os"
-
 func routePrefix() string {
-	// As of go 1.23
+	// As of go 1.23 we will panic if we don't prefix with "GET "
 	// https://github.com/golang/go/blob/9fcffc53593c5cd103630d0d24ef8bd91e17246d/src/net/http/pprof/pprof.go#L98-L97
 	// https://github.com/golang/go/commit/9fcffc53593c5cd103630d0d24ef8bd91e17246d
-	prefix := ""
-	//if godebug.New("httpmuxgo121").Value() != "1" { // todo, how to check it?
-	if os.Getenv("PYROSCOPE_GODELTAPROF_HTTPMUXGO121") != "1" {
-		prefix = "GET "
-	}
-	return prefix
-
+	return "GET "
 }
